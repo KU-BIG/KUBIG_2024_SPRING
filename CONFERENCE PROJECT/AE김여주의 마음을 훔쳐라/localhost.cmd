4ca7f8e15abba9ef1069c4c0cc88e17dd2423083
Haewon Jung <ttom0505@hanmail.net>
2024-06-30 19:23:04 +0900

    Update localhost.cmd

--- CONFERENCE PROJECT/AE김여주의 마음을 훔쳐라/localhost.cmd
+++ CONFERENCE PROJECT/AE김여주의 마음을 훔쳐라/localhost.cmd
@@ -1,4 +1,5 @@
-# 만든 챗봇 py 파일을 다음 코드를 통해 로컬에서 먼저 실행
+# 만든 챗봇 py 파일을 명령프롬프트(cmd)에서 불러오는 과정
+# 다음 코드를 통해 로컬에서 먼저 실행
 
 C:\Users\user>cd C:/Program Files/ngrok # 파일 경로 설정
 C:\Program Files\ngrok>streamlit run chatwithkimyeoju_app.py # 앱 실행
